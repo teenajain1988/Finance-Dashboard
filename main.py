a00cccda7be89ef8442ec61868aa1445d769f840
Teena Jain <75884102+teenajain1988@users.noreply.github.com>
2022-03-09 05:39:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,7 @@
 
 
 
-# First Milestone Project: Develop a Stock Ticker
-# dashboard that either allows the user to enter
-# a ticker symbol into an input box, or to select
-# item(s) from a dropdown list, and uses pandas_datareader
-# to look up and display stock data on a graph.
-######
-# EXPAND STOCK SYMBOL INPUT TO PERMIT MULTIPLE STOCK SELECTION
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
